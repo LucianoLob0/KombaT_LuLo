83819378a5fbdace59ce897ebf3bd233c8ad9626
Luciano Lobo <lloboromero@frba.utn.edu.ar>
2021-04-23 00:55:11 -0300

    Prueba1

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,15 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+poderDeGolpe :: Number -> Number
+
+poderDeGolpe horas = 15 * horas
+
+fortalezaDelObjetivo :: String -> Number
+
+fortalezaDelObjetivo nombre = 2 * length nombre 
+
+presionDelGolpe :: Number -> String -> Number
+
+presionDelGolpe a b = poderDeGolpe a / fortalezaDelObjetivo b
